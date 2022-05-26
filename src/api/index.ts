1add80fe808cfbbb6bce6b3998cfa46ecb9336e1
luodezhong <luodezhong613@hellobike.com>
2022-05-26 21:07:56 +0800

    fix: 添加基金数据和接口

---
+++ src/api/index.ts
@@ -0,0 +1,35 @@
+import Taro from "@tarojs/taro"
+
+
+export const getFundInfo = (code: string, startDate?: string) => {
+  return new Promise((resolve, reject) => {
+    Taro.request({
+      url: `https://api.doctorxiong.club/v1/fund/detail?code=${code}&startDate=${startDate}`,
+      success: (res) => { 
+        if (res.statusCode === 200) {
+          resolve(res.data)
+        } else {
+          reject(res)
+        }
+       },
+      fail: (error) => { reject(error) }
+    })
+  })
+}
+
+
+export const getFundInfoList = (codeList: string[], startDate?: string) => {
+  return new Promise((resolve, reject) => {
+    Taro.request({
+      url: `https://api.doctorxiong.club/v1/fund/detail/list?code=${codeList.join(',')}&startDate=${startDate}`,
+      success: (res) => { 
+        if (res.statusCode === 200) {
+          resolve(res.data)
+        } else {
+          reject(res)
+        }
+       },
+      fail: (error) => { reject(error) }
+    })
+  })
+}
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,7 +1,8 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
 import { View, Text, Button, Image } from "@tarojs/components";
 import { useEnv, useNavigationBar, useModal, useToast } from "taro-hooks";
-import logo from "./hook.png";
+import { getFundInfo, getFundInfoList } from '../../api/index';
+import dayjs from 'dayjs';
 
 import './index.less'
 
@@ -23,22 +24,60 @@ const Index = () => {
     });
   }, [show, showToast]);
 
+  const fundCodeArr = ['003095', '161725', '005827', '110020', '011103', '002190', '001475', '110011'] 
+  const fundBuyInfo = {
+    '003095': {
+      lastBuyPrice: '2.5080',
+      avgPrice: '3.1320'
+    },
+    '161725': {
+      lastBuyPrice: '1.0302',
+      avgPrice: '1.2053'
+    },
+    '005827': {
+      lastBuyPrice: '2.4799',
+      avgPrice: '2.6587'
+    },
+    '110020': {
+      lastBuyPrice: '1.7569',
+      avgPrice: '1.8808'
+    },
+    '011103': {
+      lastBuyPrice: '1.2092',
+      avgPrice: '1.3628'
+    },
+    '002190': {
+      lastBuyPrice: '3.3694',
+      avgPrice: '3.9964'
+    },
+    '001475': {
+      lastBuyPrice: '1.5420',
+      avgPrice: '1.6722'
+    },
+    '110011': {
+      lastBuyPrice: '6.0289',
+      avgPrice: '8.6525'
+    }
+  }
+
+  useEffect(() => {
+    const today = dayjs().format('YYYY-MM-DD')
+    getFundInfoList(fundCodeArr, today).then((res) => {
+      console.log(res)
+    })
+  }) 
+
   return (
-    <View className="wrapper">
-      <Image className="logo" src={logo} />
-      <Text className="title">为Taro而设计的Hooks Library</Text>
-      <Text className="desc">
-        目前覆盖70%官方API. 抹平部分API在H5端短板. 提供近40+Hooks!
-        并结合ahook适配Taro!
-      </Text>
-      <View className="list">
-        <Text className="label">运行环境</Text>
-        <Text className="note">{env}</Text>
+    <View className='wrapper'>
+      
+      <View className='list'>
+        <Text className='label'>运行环境</Text>
+        <Text className='note'>{env}</Text>
       </View>
-      <Button className="button" onClick={() => setTitle("Taro Hooks Nice!")}>
+      <Button className='button' onClick={() => setTitle("Taro Hooks Nice!")}>
         设置标题
       </Button>
-      <Button className="button" onClick={handleModal}>
+      <Button className='button' onClick={handleModal}>
         使用Modal
       </Button>
     </View>
